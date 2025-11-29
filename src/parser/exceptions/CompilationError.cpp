aac4d88f109aaffbafb3ba7861d77dabcb67bd07
agamjeetsingh <agamjeet.singh24@imperial.ac.uk>
2025-11-29 12:39:20 +0000

    remove: Deleted no-longer-needed compilation error class

--- src/parser/exceptions/CompilationError.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by Agamjeet Singh on 24/11/25.
-//
-
-#include "../../../include/diagnostics/CompilationError.h"
-
-const char *CompilationError::what() const noexcept {
-    return msg.c_str();
-}
