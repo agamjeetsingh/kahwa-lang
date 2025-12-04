d5671cd44e0c13a424c48727ee3b11f672c5f5a3
agamjeetsingh <agamjeet.singh24@imperial.ac.uk>
2025-12-04 12:15:16 +0000

    feat: Added super classes in ClassDecl

--- include/parser/ClassDecl.h
+++ include/parser/ClassDecl.h
@@ -10,6 +10,7 @@
 #include "FieldDecl.h"
 #include "MethodDecl.h"
 #include "Modifier.h"
+#include "TypeRef.h"
 #include "../tokeniser/Token.h"
 
 class ClassDecl {
@@ -19,12 +20,14 @@ public:
         const SourceRange &nameSourceRange,
         const SourceRange &bodyRange,
         const std::vector<Modifier> &modifiers = {},
+        const std::vector<TypeRef*>& superClasses = {},
         const std::vector<FieldDecl*> &fields = {},
         const std::vector<MethodDecl*> &methods = {},
         const std::vector<ClassDecl*> &nestedClasses = {}
         ):
     name(std::move(name)),
     modifiers(modifiers),
+    superClasses(superClasses),
     fields(fields),
     methods(methods),
     nestedClasses(nestedClasses),
@@ -35,6 +38,7 @@ public:
 private:
     std::string name;
     std::vector<Modifier> modifiers;
+    std::vector<TypeRef*> superClasses;
     std::vector<FieldDecl*> fields;
     std::vector<MethodDecl*> methods;
     std::vector<ClassDecl*> nestedClasses;
