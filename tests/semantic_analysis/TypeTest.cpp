742c27b887906859e758596bf5ecd622e91d6420
agamjeetsingh <agamjeet.singh24@imperial.ac.uk>
2025-12-21 19:23:05 +0000

    feat: Improved testing of subtyping by adding helper method and by using builders, and wrote test for subtyping of generics with one parameter

--- tests/semantic_analysis/TypeTest.cpp
+++ tests/semantic_analysis/TypeTest.cpp
@@ -8,10 +8,26 @@
 
 class TypeTest : public testing::Test {
 protected:
-    ClassSymbol* intTypeSymbol = ClassSymbolBuilder("int").build();
-    ClassSymbol* floatTypeSymbol = ClassSymbolBuilder("float").build();
-    Type intType = Type(intTypeSymbol);
-    Type floatType = Type(floatTypeSymbol);
+    ClassSymbol* numberTypeSymbol = ClassSymbolBuilder("number").build();
+    Type* numberType = TypeBuilder(numberTypeSymbol).build();
+    ClassSymbol* intTypeSymbol = ClassSymbolBuilder("int").withSuperClass(numberType).build();
+    ClassSymbol* floatTypeSymbol = ClassSymbolBuilder("float").withSuperClass(numberType).build();
+    Type* intType = TypeBuilder(intTypeSymbol).build();
+    Type* floatType = TypeBuilder(floatTypeSymbol).build();
+
+    static void testSubtypeRelations(const std::vector<Type*>& types, const std::vector<std::pair<Type*, Type*>>& expectedSubtypes) {
+        for (Type* subtype : types) {
+            for (Type* supertype : types) {
+                if (subtype == supertype) {
+                    EXPECT_TRUE(subtype->isSubtypeOf(supertype));
+                    continue;
+                }
+                bool expected = std::ranges::find(expectedSubtypes, std::make_pair(subtype, supertype)) != expectedSubtypes.end();
+                EXPECT_EQ(subtype->isSubtypeOf(supertype), expected);
+                assert(subtype->isSubtypeOf(supertype) == expected);
+            }
+        }
+    }
 };
 
 TEST_F(TypeTest, CorrectlyChecksSubtypingForDirectInheritance) {
@@ -36,18 +52,53 @@ TEST_F(TypeTest, SameTypeIsEqualToItself) {
     };
 
     // int, A<int>, A<float, int>, A<int, U>
-    auto type = Type(TypeParameterSymbolBuilder("T").build());
     std::vector types = {
-        Type(typeSymbols[0]),
-        Type(typeSymbols[1], {&intType}),
-        Type(typeSymbols[2], {&floatType, &intType}),
-        Type(typeSymbols[3], {&intType, &type})
+        TypeBuilder(typeSymbols[0]).build(),
+        TypeBuilder(typeSymbols[1]).with(intType).build(),
+        TypeBuilder(typeSymbols[2]).with({floatType, intType}).build(),
+        TypeBuilder(typeSymbols[3]).with({intType, TypeBuilder("U").build()}).build()
     };
 
     for (const auto& type: types) {
         EXPECT_TRUE(type == type);
-        EXPECT_TRUE(type.isSubtypeOf(&type));
+        EXPECT_TRUE(type->isSubtypeOf(type));
     }
+}
+
+TEST_F(TypeTest, CorrectlyChecksSubtypingForGenericVarianceWithOneParameter) {
+    // A<T>
+    auto typeSymbolA = ClassSymbolBuilder("A").with("T").build();
+    // B<out T>
+    auto typeSymbolB = ClassSymbolBuilder("B").with("T", Variance::COVARIANT).build();
+    // C<in T>
+    auto typeSymbolC = ClassSymbolBuilder("C").with("T", Variance::CONTRAVARIANT).build();
+
+    auto typeAint = TypeBuilder(typeSymbolA).with(intType).build(); // A<int>
+    auto typeAfloat = TypeBuilder(typeSymbolA).with(floatType).build(); // A<float>
+    auto typeAnumber = TypeBuilder(typeSymbolA).with(numberType).build(); // A<number>
+
+    auto typeBint = TypeBuilder(typeSymbolB).with(intType).build(); // B<int>
+    auto typeBfloat = TypeBuilder(typeSymbolB).with(floatType).build(); // B<float>
+    auto typeBnumber = TypeBuilder(typeSymbolB).with(numberType).build(); // B<number>
+
+    auto typeCint = TypeBuilder(typeSymbolC).with(intType).build(); // C<int>
+    auto typeCfloat = TypeBuilder(typeSymbolC).with(floatType).build(); // C<float>
+    auto typeCnumber = TypeBuilder(typeSymbolC).with(numberType).build(); // C<number>
+
+    // A is invariant in its generic parameter, so no subtyping
+    testSubtypeRelations({typeAint, typeAfloat, typeAnumber}, {});
+
+    // B is covariant so only B<int> <: B<number> and B<float> <: B<number> should hold
+    testSubtypeRelations({typeBint, typeBfloat, typeBnumber}, {
+        {typeBint, typeBnumber},
+        {typeBfloat, typeBnumber}
+    });
+
+    // C is covariant so only C<number> <: C<int> and C<number> <: C<float> should hold
+    testSubtypeRelations({typeCint, typeCfloat, typeCnumber}, {
+        {typeCnumber, typeCint},
+        {typeCnumber, typeCfloat}
+    });
 }
 
 TEST_F(TypeTest, CorrectlyChecksSubtypingForChainOfInheritance) {
@@ -60,29 +111,39 @@ TEST_F(TypeTest, CorrectlyChecksSubtypingForChainOfInheritance) {
     auto typeB = TypeBuilder(typeSymbolB).build();
     auto typeA = TypeBuilder(typeSymbolA).build();
 
-    EXPECT_TRUE(typeA->isSubtypeOf(typeB));
-    EXPECT_TRUE(typeB->isSubtypeOf(typeC));
-    EXPECT_TRUE(typeA->isSubtypeOf(typeC));
-    EXPECT_FALSE(typeB->isSubtypeOf(typeA));
-    EXPECT_FALSE(typeC->isSubtypeOf(typeB));
-    EXPECT_FALSE(typeC->isSubtypeOf(typeA));
+    testSubtypeRelations({typeA, typeB, typeC}, {
+        {typeA, typeB},
+        {typeB, typeC},
+        {typeA, typeC}
+    });
 }
 
-TEST_F(TypeTest, CorrectlyChecksSubtypingForDirectInheritanceWithGenerics) {
+TEST_F(TypeTest, CorrectlyChecksSubtypingForDirectInheritanceWithGenericsHavingSingleParameter) {
     // B<T>
     auto typeSymbolB = ClassSymbolBuilder("B").with("T").build();
     // B<int>
-    auto typeB1 = TypeBuilder(typeSymbolB).with(&intType).build();
+    auto typeB1 = TypeBuilder(typeSymbolB).with(intType).build();
     // B<U>
     auto typeB2 = TypeBuilder(typeSymbolB).with(TypeBuilder("U").build()).build();
 
     // A<U>
     auto typeSymbolA = ClassSymbolBuilder("A").with("U").withSuperClass(typeB2).build();
     // A<int>
-    auto typeA1 = TypeBuilder(typeSymbolA).with(&intType).build();
+    auto typeA1 = TypeBuilder(typeSymbolA).with(intType).build();
     // A<U>
     auto typeA2 = TypeBuilder(typeSymbolA).with(TypeBuilder("U").build()).build();
 
-    EXPECT_TRUE(typeA1->isSubtypeOf(typeB1)); // B<int> <: A<int>
-    EXPECT_TRUE(typeA2->isSubtypeOf(typeB2)); // A<U> <: B<U>
+    testSubtypeRelations({typeA1, typeA2, typeB1, typeB2}, {
+        {typeA1, typeB1}, // A<int> <: B<int>
+        {typeA2, typeB2}  // A<U> <: B<U>
+    });
+
+    // B<float>
+    auto typeB3 = TypeBuilder(typeSymbolB).with(floatType).build();
+    // B<V>
+    auto typeB4 = TypeBuilder(typeSymbolB).with(TypeBuilder("V").build()).build();
+
+    // False: A<int> <: B<float> (B's variance is invariant)
+    // False: A<U> <: B<V> (unrelated generic types)
+    testSubtypeRelations({typeA1, typeA2, typeB3, typeB4}, {});
 }
