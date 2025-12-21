1239b7100fdaf3394255f149fa495fcca7b93333
agamjeetsingh <agamjeet.singh24@imperial.ac.uk>
2025-12-21 13:11:18 +0000

    feat: Implemented Variance enum for types

---
+++ include/types/Variance.h
@@ -0,0 +1,18 @@
+//
+// Created by Agamjeet Singh on 20/12/25.
+//
+
+#ifndef VARIANCE_H
+#define VARIANCE_H
+
+
+
+enum class Variance {
+    COVARIANT,
+    CONTRAVARIANT,
+    INVARIANT
+};
+
+
+
+#endif //VARIANCE_H
