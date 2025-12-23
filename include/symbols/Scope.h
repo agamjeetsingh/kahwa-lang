d8eb0601a37c38a0e25742e6de67e77bb29f796c
agamjeetsingh <agamjeet.singh24@imperial.ac.uk>
2025-12-23 16:21:09 +0000

    feat: Symbol table now stores list of symbols for each string, and added helper methods for defining symbols, and adding outer scopes and for searching the current scope

--- include/symbols/Scope.h
+++ include/symbols/Scope.h
@@ -9,26 +9,55 @@
 
 #include "Symbol.h"
 
-typedef std::unordered_map<std::string, Symbol*> SymbolTable;
+typedef std::unordered_map<std::string, std::vector<Symbol*>> SymbolTable;
 
 struct Scope {
-    SymbolTable table;
+    [[nodiscard]] const std::vector<Symbol*>& searchCurrent(const std::string& name) {
+        return table[name];
+    }
 
-    std::vector<Scope*> outerScopes;
+    [[nodiscard]] std::optional<Symbol*> searchCurrentUnique(const std::string& name) const {
+        const auto& symbols = search(name);
+        return symbols.size() == 1 ? std::optional(symbols[0]) : std::nullopt;
+    }
 
-    Symbol* search(const std::string& name) const {
+    [[nodiscard]] std::vector<Symbol*> search(const std::string& name) const {
         if (table.contains(name)) {
             return table.at(name);
         }
 
         for (auto scope: outerScopes) {
-            if (auto* symbol = scope->search(name)) {
-                return symbol;
+            if (const auto& symbols = scope->search(name); !symbols.empty()) {
+                return symbols;
             }
         }
 
-        return nullptr;
+        return {};
     }
+
+    void define(Symbol* symbol) {
+        table[symbol->name].push_back(symbol);
+    }
+
+    template <typename T>
+    requires std::derived_from<T, Symbol>
+    void defineAll(const std::vector<T*>& symbols) {
+        std::ranges::for_each(symbols, [this](auto* t) {
+            table[t->name].push_back(t);
+        });
+    }
+
+    void addOuterScope(Scope* outerScope) {
+        outerScopes.push_back(outerScope);
+    }
+
+    void addOuterScopes(const std::vector<Scope*>& outerScopes) {
+        this->outerScopes.insert(this->outerScopes.begin(), outerScopes.begin(), outerScopes.end());
+    }
+private:
+    SymbolTable table;
+
+    std::vector<Scope*> outerScopes;
 };
 
 
