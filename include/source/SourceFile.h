238f52c2976e215166fc0ed4bae056e2e8f1e17e
agamjeetsingh <agamjeet.singh24@imperial.ac.uk>
2025-12-30 19:22:26 +0000

    feat: Implemented line pos caching in Source File

--- include/source/SourceFile.h
+++ include/source/SourceFile.h
@@ -4,12 +4,35 @@
 
 #ifndef SOURCEFILE_H
 #define SOURCEFILE_H
-#include <filesystem>
+
 
 
 struct SourceFile {
-    const std::filesystem::path path;
+    explicit SourceFile(const std::string& contents): contents(contents) {}
+
     const std::string contents;
+
+    std::pair<std::size_t, std::size_t> getLinePos(std::size_t offset) const {
+        if (!newlineCacheBuilt) {
+            newlinePositions.reserve(contents.size() / 16);
+            for (std::size_t i = 0; i < contents.size(); ++i) {
+                if (contents[i] == '\n') {
+                    newlinePositions.push_back(i);
+                }
+            }
+            newlineCacheBuilt = true;
+        }
+
+        auto it = std::ranges::upper_bound(newlinePositions, offset);
+        std::size_t line = it - newlinePositions.begin();
+        std::size_t lineStart = (line == 0) ? 0 : newlinePositions[line - 1] + 1;
+        std::size_t column = offset - lineStart;
+        return {line, column};
+    }
+
+private:
+    mutable bool newlineCacheBuilt = false;
+    mutable std::vector<std::size_t> newlinePositions;
 };
 
 
