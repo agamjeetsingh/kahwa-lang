b0d402e46ef1c87ccaafe6c9ec8c62402557024c
agamjeetsingh <agamjeet.singh24@imperial.ac.uk>
2026-01-08 19:22:45 +0000

    feat: Wrote tests for parsing of top-level functions and in-class methods

--- tests/parser/FunctionTest.cpp
+++ tests/parser/FunctionTest.cpp
@@ -9,41 +9,166 @@
 #include "../../include/parser/Parser.h"
 #include "../../include/tokeniser/Tokeniser.h"
 #include "../../include/parser/Modifier.h"
-#include "ParserTestBase.cpp"
+#include "StmtTestFixture.h"
+#include "../../include/symbols/TypeParameterSymbol.h"
 
-class FunctionTest : public testing::Test, public ParserTestBase {};
+class FunctionTest : public StmtTestFixture {
+protected:
+    void testFunctions(const std::vector<std::pair<std::string, MethodDecl*>>& tests) const {
+        for (auto& [str, expectedMethod]: tests) {
+            auto parsedMethod = parseMethod(str);
+            EXPECT_PRED2(methodDeclEqualIgnoreSourceRange, parsedMethod, expectedMethod);
+            std::cout << "parsedMethod: " << toString(parsedMethod) << "\nexpectedMethod: " << toString(expectedMethod) << "\n--------" << std::endl;
+        }
+    }
+
+    void testFunctions(const std::vector<MethodDecl*>& methods) const {
+        for (auto* expectedMethod: methods) {
+            if (expectedMethod->typeParameters.size() > 0) {
+
+            }
+            auto parsedMethod = parseMethod(toString(expectedMethod));
+            EXPECT_PRED2(methodDeclEqualIgnoreSourceRange, parsedMethod, expectedMethod);
+            std::cout << "parsedMethod: " << toString(parsedMethod) << "\nexpectedMethod: " << toString(expectedMethod) << "\n--------" << std::endl;
+        }
+    }
+
+    void testClasses(const std::vector<ClassDecl*>& classes) const {
+        for (auto* expectedClass: classes) {
+            auto parsedClass = parseClass(toString(expectedClass));
+            EXPECT_PRED2(classDeclEqualIgnoreSourceRange, parsedClass, expectedClass);
+            std::cout << "parsedClass: " << toString(parsedClass) << "\nexpectedClass: " << toString(expectedClass) << "\n--------" << std::endl;
+        }
+    }
+};
 
 TEST_F(FunctionTest, ParsesTopLevelMethodsCorrectly) {
-    const auto methodDecl1 = MethodDeclBuilder(
-        "foo",
-        TypeRefBuilder("float").build(),
-        BlockBuilder().build()).build();
-    const auto str1 = toString(methodDecl1);
+    std::vector<MethodDecl*> functions;
 
-    EXPECT_PRED2(kahwaFileEqualIgnoreSourceRange,
-        parseFile(str1),
-        KahwaFileBuilder().with(methodDecl1).build());
-
-    const auto methodDecl2 = MethodDeclBuilder(
-        "foo",
-        TypeRefBuilder("int").build(),
-        BlockBuilder().build())
-        .with(Modifier::PRIVATE)
-        .with(Modifier::STATIC)
-        .with({TypeRefBuilder("int").build(), "length"})
-        .with({TypeRefBuilder("vector") // vector<int>
+    std::vector modifiers = {Modifier::PRIVATE, Modifier::STATIC};
+    std::vector<std::pair<TypeRef*, std::string>> params = {
+        {TypeRefBuilder("int").build(), "length"},
+        {TypeRefBuilder("vector") // vector<int>
             .with(TypeRefBuilder("int").build())
-            .build(), "list"})
-        .build();
-    const auto str2 = toString(methodDecl2);
+            .build(), "list"}
+    };
+    std::vector<Expr*> defaultExprs = {nullptr};
+    defaultExprs.insert(defaultExprs.end(), exampleExprs.begin(), exampleExprs.end());
 
-    EXPECT_PRED2(kahwaFileEqualIgnoreSourceRange,
-        parseFile(str2),
-        KahwaFileBuilder().with(methodDecl2).build());
+    for (int i = 0; i <= modifiers.size(); i++) {
+        for (int j = 0; j <= params.size(); j++) {
+            for (auto body: exampleBlocks) {
+                for (auto defaultExpr: defaultExprs) {
+                    std::vector<Modifier> modifierSubset{modifiers.begin(), modifiers.begin() + i};
+                    std::vector<FieldDecl*> paramSubset;
+                    std::transform(params.begin(), params.begin() + j, std::back_inserter(paramSubset), [defaultExpr](const std::pair<TypeRef*, std::string>& pair) {
+                        return FieldDeclBuilder(pair.second, pair.first).with(defaultExpr).build();
+                    });
 
-    EXPECT_PRED2(kahwaFileEqualIgnoreSourceRange,
-        parseFile(str1 + str2),
-        KahwaFileBuilder().with({methodDecl1, methodDecl2}).build());
+                    functions.push_back(MethodDeclBuilder("foo", TypeRefBuilder("int").build(),
+                        body)
+                        .with(modifierSubset)
+                        .with(paramSubset)
+                        .build());
+                }
+            }
+        }
+    }
+
+    testFunctions(functions);
+
+    expectNoDiagnostics();
+}
+
+TEST_F(FunctionTest, ParsesClassMethodsCorrectly) {
+    std::vector<MethodDecl*> functions;
+
+    std::vector modifiers = {Modifier::PRIVATE, Modifier::STATIC};
+    std::vector<std::pair<TypeRef*, std::string>> params = {
+        {TypeRefBuilder("int").build(), "length"},
+        {TypeRefBuilder("vector") // vector<int>
+            .with(TypeRefBuilder("int").build())
+            .build(), "list"}
+    };
+    std::vector<Expr*> defaultExprs = {nullptr};
+    defaultExprs.insert(defaultExprs.end(), exampleExprs.begin(), exampleExprs.end());
+
+    for (int i = 0; i <= modifiers.size(); i++) {
+        for (int j = 0; j <= params.size(); j++) {
+            for (auto body: exampleBlocks) {
+                for (auto defaultExpr: defaultExprs) {
+                    std::vector<Modifier> modifierSubset{modifiers.begin(), modifiers.begin() + i};
+                    std::vector<FieldDecl*> paramSubset;
+                    std::transform(params.begin(), params.begin() + j, std::back_inserter(paramSubset), [defaultExpr](const std::pair<TypeRef*, std::string>& pair) {
+                        return FieldDeclBuilder(pair.second, pair.first).with(defaultExpr).build();
+                    });
+
+                    functions.push_back(MethodDeclBuilder("foo", TypeRefBuilder("int").build(),
+                        body)
+                        .with(modifierSubset)
+                        .with(paramSubset)
+                        .build());
+                }
+            }
+        }
+    }
+
+    std::vector<ClassDecl*> classes;
+
+    for (int i = 0; i < std::min(functions.size(), static_cast<std::size_t>(300)); i++) {
+        classes.push_back(ClassDeclBuilder("bar").with(std::vector(functions.begin(), functions.begin() + i + 1)).build());
+    }
+
+    testClasses(classes);
+
+    expectNoDiagnostics();
+}
+
+// TODO - Check Generics
+
+TEST_F(FunctionTest, ParsesFunctionsWithGenericsCorrectly) {
+    std::vector<MethodDecl*> functions;
+
+    std::vector modifiers = {Modifier::PRIVATE, Modifier::STATIC};
+    std::vector<std::pair<TypeRef*, std::string>> params = {
+        {TypeRefBuilder("int").build(), "length"},
+        {TypeRefBuilder("vector") // vector<int>
+            .with(TypeRefBuilder("int").build())
+            .build(), "list"}
+    };
+    std::vector<Expr*> defaultExprs = {nullptr};
+    defaultExprs.insert(defaultExprs.end(), exampleExprs.begin(), exampleExprs.end());
+
+    std::vector genericParameters = {
+        astArena.make<TypeParameterDecl>(dummy_source, "T", Variance::INVARIANT),
+        astArena.make<TypeParameterDecl>(dummy_source, "U", Variance::INVARIANT),
+        astArena.make<TypeParameterDecl>(dummy_source, "V", Variance::INVARIANT)
+    };
+
+    for (int i = 0; i <= modifiers.size(); i++) {
+        for (int j = 0; j <= params.size(); j++) {
+            for (auto body: exampleBlocks) {
+                for (auto defaultExpr: defaultExprs) {
+                    for (int k = 0; k <= genericParameters.size(); k++) {
+                        std::vector<Modifier> modifierSubset{modifiers.begin(), modifiers.begin() + i};
+                        std::vector<FieldDecl*> paramSubset;
+                        std::transform(params.begin(), params.begin() + j, std::back_inserter(paramSubset), [defaultExpr](const std::pair<TypeRef*, std::string>& pair) {
+                            return FieldDeclBuilder(pair.second, pair.first).with(defaultExpr).build();
+                        });
+                        std::vector<TypeParameterDecl*> genericParameterSubset{genericParameters.begin(), genericParameters.begin() + k};
+                        functions.push_back(MethodDeclBuilder("foo", TypeRefBuilder("int").build(),
+                            body)
+                            .with(modifierSubset)
+                            .with(paramSubset)
+                            .with(genericParameterSubset)
+                            .build());
+                    }
+                }
+            }
+        }
+    }
+
+    testFunctions(functions);
 
     expectNoDiagnostics();
 }
